7c87d3d85dde93e8b54c9199ad72ff8b0d3517d1
peach lasagna <kir.kud@inbox.ru>
2021-01-10 01:15:07 +0700

    работай пазязя

--- hamcode/decoder.py
+++ hamcode/decoder.py
@@ -4,30 +4,27 @@ from typing import Union
 
 def array_count_xor(array: list[int], kbit_num: int):
     n = 0
-    for i in range(kbit_num, len(array), kbit_num*2):
-        for j in array[i:i+kbit_num]:
+    for i in range(2**(kbit_num - 1), len(array) + 1, 2**kbit_num):
+        for j in array[i - 1:i - 1 + 2**(kbit_num - 1)]:
             n ^= j
     return n
 
-
 def decode_block(array: list[int], k: int):
-    dct = {}
     n = 1
+    bit_err_ind = -1
+    n_mas =[]
 
     for i in range(1, k+1):
-        dct.update({n: array_count_xor(array, i)})
+        bit_err_ind += (n-1) * (array_count_xor(array, i) == array[n-1])
+        n_mas.append(n)
         n *= 2
-
-    bit_err_ind = -1
-
-    for key, v in dct.items():
-        bit_err_ind += (key-1) * (v == array[key-1])
 
     if bit_err_ind != -1:
         bit_err_ind +=1
         array[bit_err_ind] ^= 1
     n = -1
-    for key in dct.keys():
+
+    for key in n_mas:
         n+=1
         del array[key-1-n]
     return array, bit_err_ind if bit_err_ind != -1 else None
@@ -40,15 +37,20 @@ def decode(string: str, block_len: int, return_bit_errors: bool= False) -> Union
     array = list(map(int, list(string)))
     k = count_k(block_len)
     block_len += k
-    chunks = [array[i - block_len:i] for i in range(block_len, len(array)+1, block_len)]
     err_bits = []
     res = ''
-    for ind in range(len(chunks)):
-        el, err_bit = decode_block(chunks[ind], k)
+
+    n = -1
+    for ind in range(block_len, len(array)+1, block_len):
+        n+=1
+        el, err_bit = decode_block(array[ind-block_len:ind], k)
+
         if return_bit_errors and err_bit is not None:
-            err_bits.append(err_bit*(ind+1))
+            err_bits.append(err_bit*(n+1))
         res+= ''.join(str(e) for e in el)
+
     if return_bit_errors:
         return res, err_bits
     return res
+
 
