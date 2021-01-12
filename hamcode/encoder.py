bdaf4667b1e1d8416bb9296a028178fa4a8aa0ae
peach lasagna <kir.kud@inbox.ru>
2021-01-12 20:33:03 +0700

    encode fix

--- hamcode/encoder.py
+++ hamcode/encoder.py
@@ -1,12 +1,7 @@
 from .decoder import count_k, array_count_xor
 
 
-def encode(string: str, block_len: int) -> str:
-    """
-    In Ham-Code (12,8) block_len == 8
-    """
-    k = count_k(block_len)
-    array = list(map(int, string))
+def encode_block(array: list[int], k: int):
     kbit_mas = []
     for i in range(1, k+1):
         b = 2**i//2
@@ -15,10 +10,19 @@ def encode(string: str, block_len: int) -> str:
 
     for kbit in kbit_mas:
         array[kbit-1] = array_count_xor(array, kbit)
+    return array
 
+def encode(string: str, block_len: int) -> str:
+    """
+    In Ham-Code (12,8) block_len == 8
+    """
     res = ''
-    for i in array:
-        res += str(i)
-    return res
+    k = count_k(block_len)
+    array = list(map(int, string))
 
+    for ind in range(block_len, len(array)+1, block_len):
+        el = encode_block(array[ind-block_len:ind], k)
+        res += ''.join(str(e) for e in el)
+
+    return res
 
